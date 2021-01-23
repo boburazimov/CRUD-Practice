3b79f8aa009a34ffe33202de75f6d8f79b04ae3a
boburazimov <uzbek.4103@gmail.com>
2021-01-23 13:57:30 +0500

    First Entity

--- crud-practice-server/src/main/java/uz/sav/crud/CrudPracticeApplication.java
+++ crud-practice-server/src/main/java/uz/sav/crud/CrudPracticeApplication.java
@@ -3,11 +3,12 @@ package uz.sav.crud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Scanner;
+
 @SpringBootApplication
 public class CrudPracticeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CrudPracticeApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(CrudPracticeApplication.class, args);
+    }
 }
