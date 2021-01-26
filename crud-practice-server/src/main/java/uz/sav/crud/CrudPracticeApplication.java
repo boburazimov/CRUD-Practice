030530da52c5ffe4125df78a843ab57e73b66a9b
Og'abek Qodirov <qodirovbek1996@gmail.com>
2021-01-26 18:24:51 +0500

    First Entity

--- crud-practice-server/src/main/java/uz/sav/crud/CrudPracticeApplication.java
+++ crud-practice-server/src/main/java/uz/sav/crud/CrudPracticeApplication.java
@@ -3,12 +3,13 @@ package uz.sav.crud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Scanner;
+
 
 @SpringBootApplication
 public class CrudPracticeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CrudPracticeApplication.class, args);
-    }
+
+               }
 }
--- crud-practice-server/src/main/java/uz/sav/crud/entity/Position.java
+++ crud-practice-server/src/main/java/uz/sav/crud/entity/Position.java
@@ -5,10 +5,16 @@ import lombok.EqualsAndHashCode;
 import uz.sav.crud.entity.template.TemplateNameModel;
 
 import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
 @Entity
+@Table(name = "position")
 public class Position extends TemplateNameModel {
+
+//    @OneToOne(mappedBy = "employee")
+//    private Employee employee;
 
 }
